bf9914c1b9a23584653db7296daede7eae87d7e8
Tahrima-Banna <tahrimabannadr23@gmail.com>
2024-10-04 21:25:51 +0600

    Add files via upload

---
+++ floor_element.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int floor(int a[],int n,int x){
+
+    int l=0, r=n-1;
+    int ans=0;
+    while(l<=r){
+
+        int mid = (l+r)/2;
+
+        if(a[mid]== x){
+            ans=mid;
+            break;
+        }
+        else if(a[mid]<x){
+            ans= a[mid];
+            l=mid+1;
+        }
+        else{
+            r=mid-1;
+        }
+    }
+    return ans;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+         cin>>a[i];
+    }
+    int x;
+    cin>>x;
+    int idx=floor(a,n,x);
+    cout<<idx<<endl;
+    return 0;
+}
