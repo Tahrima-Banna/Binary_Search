cf027695b5f6283620bf542d9e9e1dd78252a3f5
Tahrima-Banna <tahrimabannadr23@gmail.com>
2024-10-05 20:42:24 +0600

    Add files via upload

---
+++ next_alpha.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std ;
+char slove(string s, char ch, int n)
+{
+    int l=0;
+    int r=n-1;
+    char ans;
+    while(l<=r){
+     int mid=(l+r)/2;
+
+     if(s[mid]>ch){
+
+        ans= s[mid];
+        r= mid-1;
+     }
+     else{
+        l =mid+1;
+     }
+    }
+
+    return ans;
+}
+int main()
+{
+    string s;
+    cin>>s;
+    char ch;
+    cin>>ch;
+    char ans = slove(s,ch,s.size());
+    cout<<ans<<endl;
+    return 0;
+}
